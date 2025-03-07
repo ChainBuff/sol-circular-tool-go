bda1dc8f12225584ab4190e3918b8adfae1b301c
caca <icaca@live.com>
2025-03-07 09:09:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@
 mkdir -p build
 
 # 编译 Linux 版本
-GOOS=linux GOARCH=amd64 go build -o build/sol-circular-tool
+GOOS=linux GOARCH=amd64 go build -o build/circular
 echo "Linux 版本编译完成"
 
 echo "所有版本编译完成！"
-echo "使用方法: ./sol-circular-tool -j <Jupiter API URL> -k <API Key> [选项]"
+echo "使用方法: ./circular -j <Jupiter API URL> -k <API Key> [选项]"
 echo "可选参数:"
 echo "  -i, --dex-program-ids <ID1,ID2,...>         只包含指定的DEX程序ID"
 echo "  -e, --exclude-dex-program-ids <ID1,ID2,...> 排除指定的DEX程序ID" 
